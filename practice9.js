f953f59e1253dbd44351a9ecf7c5399bffb101bf
hsprime85 <hsprime85@gmail.com>
2020-11-13 13:05:10 -0800

    datastructure sorting

---
+++ practice9.js
@@ -0,0 +1,227 @@
+/*
+const letters = ['a', 'b', 'z', 'e', 'r', 'b'];
+const basket = [2, 65, 34, 2, 1, 7, 8];
+
+basket.sort(function (a,b){
+    return a - b;
+});
+*/
+
+/*
+
+// bubble sort
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(array) {
+    const len = array.length
+    for(let i = 0; i < len; i++){
+        for(let j = 0; j < len; j++){
+            if(array[j] > array[j+1]){
+                // swap nums
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+}
+
+bubbleSort(numbers);
+console.log(numbers);
+
+*/
+
+/*
+
+// Selection sort
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function selectionSort(arr){
+    const len = arr.length;
+    for(let i = 0; i < len; i++){
+        let min = i;
+        let temp = arr[i];
+        for(let j = i+1; j < len; j++){
+            if(arr[j] < arr[min]){
+                min = j;
+            }
+        }
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return arr;
+}
+selectionSort(numbers);
+console.log(numbers);
+
+*/
+
+/*
+// insert sort
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array) {
+  const length = array.length;
+	for (let i = 0; i < length; i++) {
+		if (array[i] < array[0]) {
+      //move number to the first position
+      array.unshift(array.splice(i,1)[0]);
+    } else {
+      // only sort number smaller than number on the left of it. This is the part of insertion sort that makes it fast if the array is almost sorted.
+      if (array[i] < array[i-1]) {
+        //find where number should go
+        for (var j = 1; j < i; j++) {
+          if (array[i] >= array[j-1] && array[i] < array[j]) {
+            //move number to the right spot
+            array.splice(j,0,array.splice(i,1)[0]);
+          }
+        }
+      }
+    }
+	}
+}
+
+insertionSort(numbers);
+console.log(numbers);
+
+function insertionSort(arr) {
+
+    for (var i = 1; i < arr.length; i++) {
+        var temp = arr[i];
+        let j = i - 1
+        while (j >= 0 && temp < arr[j]) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+    return arr;
+}
+console.log(insertionSort([3, 5, 1, 2, 4]))
+
+
+function insertionSortDestructuredSwap(arr) {
+    let len = arr.length;
+    for (let i = 1; i < len; i++) {
+        // hold the index instead of the temp
+        let idx = i;
+        let j = i - 1;
+        // compare with indexes
+        while (j >= 0 && arr[j] > arr[idx]) {
+            // swap in one line
+            [arr[j], arr[idx]] = [arr[idx], arr[j]];
+            idx = j;
+            j--;
+        }
+    }
+    return arr;
+}
+*/
+
+// merge sort
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function mergeSort (array) {
+  if (array.length === 1) {
+    return array
+  }
+  // Split Array in into right and left
+  const length = array.length;
+  const middle = Math.floor(length / 2)
+  const left = array.slice(0, middle) 
+  const right = array.slice(middle)
+  // console.log('left:', left);
+  // console.log('right:', right);
+
+  
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
+}
+
+function merge(left, right){
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while(leftIndex < left.length && 
+        rightIndex < right.length){
+     if(left[leftIndex] < right[rightIndex]){
+       result.push(left[leftIndex]);
+       leftIndex++;
+     } else{
+       result.push(right[rightIndex]);
+       rightIndex++
+    }
+  }  
+  // console.log(left, right)
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+
+const answer = mergeSort(numbers);
+console.log(answer);
+
+
+
+// quick sort
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function quickSort(array, left, right){
+  const len = array.length; 
+  let pivot;
+  let partitionIndex;
+
+  if(left < right) {
+    pivot = right;
+    partitionIndex = partition(array, pivot, left, right);
+    
+    //sort left and right
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, right);
+  }
+  return array;
+}
+   
+function partition(array, pivot, left, right){
+  let pivotValue = array[pivot];
+  let partitionIndex = left;
+
+  for(let i = left; i < right; i++) {
+    if(array[i] < pivotValue){
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(array, right, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(array, firstIndex, secondIndex){
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+}
+
+//Select first and last index as 2nd and 3rd parameters
+quickSort(numbers, 0, numbers.length - 1);
+console.log(numbers);
+
+
+
+//#1 - Sort 10 schools around your house by distance:   insertion sort
+
+//#2 - eBay sorts listings by the current Bid amount:   radix or counting sort
+
+//#3 - Sport scores on ESPN     quick sort
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data     merge sort
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews   insertion sort
+
+//#6 - Temperature Records for the past 50 years in Canada  radix counting sort / quick sort
+
+//#7 - Large user name database needs to be sorted. Data is very random.  merge sort / quick sort
+
+//#8 - You want to teach sorting for the first time  bubble sort / selection sort
